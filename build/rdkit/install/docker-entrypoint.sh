e896a119156b24d60cb0527190cb18b66eab017a
Markus Sitzmann <markus.sitzmann@gmail.com>
2017-09-04 18:20:30 +0200

    initial work

--- build/rdkit/install/docker-entrypoint.sh
+++ build/rdkit/install/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Starting with UID : $APPDOCK_UID : $APPDOCK_GID : $APPDOCK_HOME : $APPDOCK_USER"
+echo "RDKIT Starting with UID : $APPDOCK_UID : $APPDOCK_GID : $APPDOCK_HOME : $APPDOCK_USER"
 groupadd -g $APPDOCK_GID $APPDOCK_USER
 useradd --shell /bin/bash -u $APPDOCK_UID -g $APPDOCK_GID -o -c "" -m $APPDOCK_USER
 export HOME=$APPDOCK_HOME
