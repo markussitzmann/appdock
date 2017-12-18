57a9314a305b60c8d0a385c79893712f083c04bb
Markus Sitzmann <markus.sitzmann@gmail.com>
2017-12-18 23:13:41 +0100

    improving docker-compose build

--- build/app/docker-entrypoint.sh
+++ build/app/docker-entrypoint.sh
@@ -5,12 +5,13 @@ echo "bla"
 if id "$APPDOCK_USER" >/dev/null 2>&1; then
     echo "User exists, skipping creation."
 else
-    echo "Creating user ..."
+    echo "Creating user and environment..."
     groupadd -g $APPDOCK_GID $APPDOCK_USER
     useradd --shell /bin/bash -u $APPDOCK_UID -g $APPDOCK_GID -o -c "" -M $APPDOCK_USER
     export HOME=$APPDOCK_HOME
     chown $APPDOCK_UID:$APPDOCK_GID /home/appdock
 fi
 
+source activate appdock
 cd /home/appdock
 exec gosu $APPDOCK_USER "$@"
