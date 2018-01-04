baa4d03f6190b6c6b1e284175d9acc954216f0c6
Markus Sitzmann <markus.sitzmann@gmail.com>
2018-01-04 16:06:31 +0100

    adding rdkit to postgres container

--- build/rdkit/install/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "RDKIT Starting with UID : $APPDOCK_UID : $APPDOCK_GID : $APPDOCK_HOME : $APPDOCK_USER"
-groupadd -g $APPDOCK_GID $APPDOCK_USER
-useradd --shell /bin/bash -u $APPDOCK_UID -g $APPDOCK_GID -o -c "" -m $APPDOCK_USER
-export HOME=$APPDOCK_HOME
-chown $APPDOCK_UID:$APPDOCK_GID /home/appdock
-
-exec gosu $APPDOCK_USER "$@"
