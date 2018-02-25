ea0347c6aff7fba6e53ac88090c4e0b1aa2354bb
Markus Sitzmann <markus.sitzmann@gmail.com>
2018-02-25 21:42:29 +0100

    working on the integration of django apps

--- context/django/project_template/appsite/appsite/urls.py
+++ context/django/project_template/appsite/appsite/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 import rest_framework.urls
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    #path(r'^api-auth/', rest_framework.urls, namespace='rest_framework')
+    path(r'admin/', admin.site.urls),
+    #path(r'api-auth/', rest_framework.urls, namespace='rest_framework')
 ]
