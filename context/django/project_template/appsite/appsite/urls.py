8f2e74ea2c086560247a5c13f6e19981c9a0dec6
Markus Sitzmann <markus.sitzmann@gmail.com>
2018-02-25 23:00:05 +0100

    clean up and switch to Django 2.0

--- context/django/project_template/appsite/appsite/urls.py
+++ context/django/project_template/appsite/appsite/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-import rest_framework.urls
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    #path(r'api-auth/', rest_framework.urls, namespace='rest_framework')
+    path('admin/', admin.site.urls),
 ]
